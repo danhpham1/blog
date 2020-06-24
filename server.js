ebab46f3fca9b028353407e4099d744ae0243889
cn16a1 <danhpham511998@gmail.com>
2020-06-24 18:22:01 +0700

    fix

--- server.js
+++ server.js
@@ -73,4 +73,4 @@ app.use("/", routerClient);
 
 //check ensureAuthenticated
 
-app.listen(3000, () => console.log("server start"));
+app.listen(process.env.PORT || 3000, () => console.log("server start"));
