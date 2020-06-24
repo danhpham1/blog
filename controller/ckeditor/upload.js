46e666deeda1cddff23d31d70a6d9f087df10668
cn16a1 <danhpham511998@gmail.com>
2020-06-24 22:42:43 +0700

    fix ckeditor

--- controller/ckeditor/upload.js
+++ controller/ckeditor/upload.js
@@ -1,20 +1,22 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-module.exports.getUploadIndex = (req,res)=>{
-    const imgs = fs.readdirSync(path.join(__dirname + '../../../public/img/post-img'));
-    // console.log(imgs);
-    res.render('ckeditor-upload/upload',{image:imgs});
-}
+module.exports.getUploadIndex = (req, res) => {
+  const imgs = fs.readdirSync(
+    path.join(__dirname + "../../../public/img/post-img")
+  );
+  // console.log(imgs);
+  res.render("ckeditor-upload/upload", { image: imgs.reverse() });
+};
 
-module.exports.processUpload = (req,res)=>{
-    res.redirect('/ckeditor/browser');
-}
+module.exports.processUpload = (req, res) => {
+  res.redirect("/admin/ckeditor/browser");
+};
 
-module.exports.processDelete = (req,res)=>{
-    let dir = path.join(__dirname + '../../../public/img/post-img');
-    if(fs.existsSync(`${dir}/${req.query.filename}`)){
-        fs.unlinkSync(`${dir}/${req.query.filename}`);
-    };
-    res.redirect('/ckeditor/browser');
-}
+module.exports.processDelete = (req, res) => {
+  let dir = path.join(__dirname + "../../../public/img/post-img");
+  if (fs.existsSync(`${dir}/${req.query.filename}`)) {
+    fs.unlinkSync(`${dir}/${req.query.filename}`);
+  }
+  res.redirect("/admin/ckeditor/browser");
+};
