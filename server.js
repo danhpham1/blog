17d8cfb63b8fc2f5c9b9a2d469ced38fbe38557b
cn16a1 <danhpham511998@gmail.com>
2020-06-24 18:07:22 +0700

    fix connect to atlas

--- server.js
+++ server.js
@@ -1,25 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const passPort = require('passport');
-const localStrategy = require('passport-local').Strategy;
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passPort = require("passport");
+const localStrategy = require("passport-local").Strategy;
 
 //require
-const routerIndex = require('./routers/admin/routerIndex');
+const routerIndex = require("./routers/admin/routerIndex");
 
-const routerUpload = require('./routers/ckeditor/upload');
+const routerUpload = require("./routers/ckeditor/upload");
 
-const routerLogin = require('./routers/admin/routerLogin');
+const routerLogin = require("./routers/admin/routerLogin");
 
-const loginMiddleware = require('./middleware/middleware-login');
+const loginMiddleware = require("./middleware/middleware-login");
 
-const routerClient = require('./routers/client/routerClient');
+const routerClient = require("./routers/client/routerClient");
 
 const app = express();
 
-//connecto to atlas
-// mongoose.connect('mongodb+srv://danh:hanhphucao@clusterblog-sbxju.mongodb.net/blog?retryWrites=true&w=majority', {
+// connecto to atlas
+mongoose.connect(
+  "mongodb+srv://danh:hanhphucao@clusterblog-sbxju.mongodb.net/blog?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  function (err) {
+    if (err) {
+      console.log("connect to mongo faild");
+    } else {
+      console.log("connect to mongo success");
+    }
+  }
+);
+//connect to local
+// mongoose.connect('mongodb://127.0.0.1:27017/blog', {
 //     useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
 // }, function (err) {
 //     if (err) {
@@ -28,43 +44,33 @@ const app = express();
 //         console.log('connect to mongo success')
 //     }
 // });
-//connect to local
-mongoose.connect('mongodb://127.0.0.1:27017/blog', {
-    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
-}, function (err) {
-    if (err) {
-        console.log('connect to mongo faild');
-    } else {
-        console.log('connect to mongo success')
-    }
-});
 
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session({
-    secret: 'blog',
+app.use(
+  session({
+    secret: "blog",
     saveUninitialized: true,
     resave: true,
-    cookie: { maxAge: 60 * 60 * 60 * 60 }
-}));
+    cookie: { maxAge: 60 * 60 * 60 * 60 },
+  })
+);
 
 app.use(passPort.initialize());
 app.use(passPort.session());
 
+app.use("/admin/index", routerLogin);
 
-app.use('/admin/index', routerLogin);
-
-app.use('/admin', loginMiddleware.loginMiddleware, routerIndex);
+app.use("/admin", loginMiddleware.loginMiddleware, routerIndex);
 //upload img to server user ckeditor
-app.use('/admin/ckeditor', routerUpload);
+app.use("/admin/ckeditor", routerUpload);
 //client
-app.use('/', routerClient);
+app.use("/", routerClient);
 
 //check ensureAuthenticated
 
-
-app.listen(3000, () => console.log('server start'));
+app.listen(3000, () => console.log("server start"));
